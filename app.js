0ea5263f6d7868bd53086f4fb0739c4c60496f7d
maharishi <bmaharishi@yahoo.com>
2017-05-27 00:40:16 +0530

    add room done,rooms grid created

--- app.js
+++ app.js
@@ -11,10 +11,15 @@ roomApp.config(function($stateProvider) {
   $mdThemingProvider.theme('default')
     .accentPalette('blue');
 });
- roomApp.controller('AppCtrl', AppCtrl)
+ roomApp.controller('AppCtrl', AppCtrl);
   function AppCtrl($scope) {
-    $scope.currentNavItem = 'home';
-    $scope.addRoom = function(){
+  	$scope.currentNavItem = 'home';
+  	var totalrooms = totalRoom(); 
+  	$scope.rooms = new Array();
+  	for(var i=totalrooms;i>0;i--)
+  		$scope.rooms.push(loadDetails(i));
+  	
+  	$scope.addRoom = function(){
     	var num = totalRoom();
     	var roomnum = 1;
     	if(!(num==null || num==0))
@@ -25,14 +30,13 @@ roomApp.config(function($stateProvider) {
     	myroom.totaltask=0;
     	localStorage.setItem('totalRooms',roomnum);
     	localStorage.setItem('room_'+roomnum,JSON.stringify(myroom));
-
-    	alert(totalRoom());
+    	$scope.rooms.unshift(myroom);
     }
-
   };
-  function addRoom($scope){
-  	
-  }
+  function loadDetails(id){
+    	return JSON.parse(localStorage.getItem('room_'+id));
+
+    }
   function totalRoom(){
   	return localStorage.getItem('totalRooms');
   }
